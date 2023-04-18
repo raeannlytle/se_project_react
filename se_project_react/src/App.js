67768e45a8c764c6c2f78d282348b093130840e0
Raeann Lytle <raelytle@gmail.com>
2023-04-18 18:14:21 -0400

    create Header and import to app.js

--- se_project_react/src/App.js
+++ se_project_react/src/App.js
@@ -1,26 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import './Header/Header.js';
 
 function App() {
   return (
     <div>
-      <header className='header'>
-        <div className='header__logo'>
-          <div>
-            <img src='/images/header-logo.svg' alt='logo'></img>
-          </div>
-          <div>Date</div>
-        </div>
-        <div className='header__avatar-logo'>
-          <div>
-            <button type='text'>Add clothes</button>
-          </div>
-          <div>Terrence Tegegne</div>
-          <div>
-            <img src='/images/header-avatar.svg' alt='avatar'></img>
-          </div>
-        </div>
-      </header>
+     <Header/>
+      <main>
+
+      </main>
     </div>
   );
 }
---
+++ se_project_react/src/Header/Header.js
@@ -0,0 +1,25 @@
+const Header = () => {
+  console.log('Header');
+
+  return(
+    <header className='header'>
+    <div className='header__logo'>
+      <div>
+        <img src='/images/header-logo.svg' alt='logo'></img>
+      </div>
+      <div>Date</div>
+    </div>
+    <div className='header__avatar-logo'>
+      <div>
+        <button type='text'>Add clothes</button>
+      </div>
+      <div>Terrence Tegegne</div>
+      <div>
+        <img src='/images/header-avatar.svg' alt='avatar'></img>
+      </div>
+    </div>
+  </header>
+  )  
+}
+
+export default Header;
