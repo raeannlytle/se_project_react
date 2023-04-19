46ad108355322931d1933caaa0c91b353d1ca0df
Raeann Lytle <raelytle@gmail.com>
2023-04-19 19:48:18 -0400

    create Main.css

--- se_project_react/src/Main/Main.js
+++ se_project_react/src/Main/Main.js
@@ -1,6 +1,7 @@
 import { defaultClothingItems } from "../utils/constants";
 import WeatherCard from '../WeatherCard/WeatherCard';
 import ItemCard from '../ItemCard/ItemCard';
+import './Main.css';
 
 function Main({ weatherTemp }) {
     return <main className='main'>
