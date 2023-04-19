c1431acbffa320b2f756a0b0a682259c3d356e18
Raeann Lytle <raelytle@gmail.com>
2023-04-19 19:21:10 -0400

    refactoring Main

---
+++ se_project_react/src/Main/Main.js
@@ -0,0 +1,15 @@
+function Main() {
+    return <main className='main'>
+      <WeatherCard day={false} type='rain' weatherTemp={weatherTemp} />
+      <section className='card_section' id='card-section'>
+        Today is {weatherTemp} / You may want to wear:
+        <div className='card_items'>
+          {defaultClothingItems.map((item) => (
+            <ItemCard item={item} />
+          ))}
+        </div>
+      </section>
+    </main>;
+  }
+
+export default Main;
