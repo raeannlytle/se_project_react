e8a8371d654455ccf61b6a599a06f1861761858e
Raeann Lytle <raelytle@gmail.com>
2023-04-20 18:17:10 -0400

    working on useState for modal

--- se_project_react/src/App.js
+++ se_project_react/src/App.js
@@ -7,36 +7,39 @@ import ModalWithForm from './ModalWithForm/ModalWithForm';
 
 function App() {
   const weatherTemp = '102°F'
+  const { activeModal, setActiveModal } = useState('');
   return (
     <div>
       <Header />
       <Main weatherTemp={weatherTemp} />
       <Footer />
-      <ModalWithForm title='New garment'>
-        <label>
-          Name
-          <input type='text' name='name' minLength='1' maxLength='30'></input>
-        </label>
-        <label>
-          Image
-          <input type='url' name='link' minLength='1' maxLength='30'></input>
-        </label>
-        <p>Select the weather type:</p>
-        <div>
+      {activeModal === 'create' && (
+        <ModalWithForm title='New garment'>
+          <label>
+            Name
+            <input type='text' name='name' minLength='1' maxLength='30'></input>
+          </label>
+          <label>
+            Image
+            <input type='url' name='link' minLength='1' maxLength='30'></input>
+          </label>
+          <p>Select the weather type:</p>
           <div>
-            <input type='radio' id='hot' value='hot'></input>
-            <label>Hot</label>
+            <div>
+              <input type='radio' id='hot' value='hot'></input>
+              <label>Hot</label>
+            </div>
+            <div>
+              <input type='radio' id='warm' value='warm'></input>
+              <label>Warm</label>
+            </div>
+            <div>
+              <input type='radio' id='cold' value='cold'></input>
+              <label>Cold</label>
+            </div>
           </div>
-          <div>
-            <input type='radio' id='warm' value='warm'></input>
-            <label>Warm</label>
-          </div>
-          <div>
-            <input type='radio' id='cold' value='cold'></input>
-            <label>Cold</label>
-          </div>
-        </div>
-      </ModalWithForm>
+        </ModalWithForm>
+      )}
     </div>
   );
 }
