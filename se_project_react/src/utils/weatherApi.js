ef30e9e32d356cebff1ff1d4a46c2b1774ad88a9
Raeann Lytle <raelytle@gmail.com>
2023-04-24 13:22:16 -0400

    export function

--- se_project_react/src/utils/weatherApi.js
+++ se_project_react/src/utils/weatherApi.js
@@ -1,7 +1,8 @@
-const getForecastWeather = () => {
-  const latitude = 44.34;
-  const longitude = 10.99;
-  const APIkey = '7f2ccea786bcf422af97ac05555347aa';
+const latitude = 44.34;
+const longitude = 10.99;
+const APIkey = '7f2ccea786bcf422af97ac05555347aa';
+
+export const getForecastWeather = () => {
   const weatherApi = fetch(
   `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`)
   .then((res) => {
