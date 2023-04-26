b450c49a40c315518560ba27df5d1bebe3d25548
Raeann Lytle <raelytle@gmail.com>
2023-04-26 12:17:29 -0400

    working on ItemModal styles

--- se_project_react/src/ItemModal/ItemModal.js
+++ se_project_react/src/ItemModal/ItemModal.js
@@ -2,7 +2,7 @@ const ItemModal = ({ selectedCard, onClose }) => {
   return (
     <div className='modal'>
       <div className='modal__content'>
-      <button type='button' onClick={onClose} className="modal__button-close-item"></button>
+      <button type='button' onClick={onClose} className="modal__button-close"></button>
         <img src={selectedCard.link} className="modal__image-preview"/>
         <div className="modal__preview-text">{selectedCard.name}</div>
         <div>Weather: {selectedCard.weather}</div>
