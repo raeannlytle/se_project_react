fe3a61d9207bb6565aac342b36dac7ec144f1bac
Raeann Lytle <raelytle@gmail.com>
2023-04-26 12:36:38 -0400

    styling header

--- se_project_react/src/Header/Header.js
+++ se_project_react/src/Header/Header.js
@@ -7,15 +7,15 @@ const Header = ({onCreateModal}) => {
       <div>
         <img src={require('../images/header-logo.svg').default} alt='logo'></img>
       </div>
-      <div>June 15, New York</div>
+      <div className='header__date'>June 15, New York</div>
     </div>
-    <div className='header__avatar-logo'>
+    <div className='header__avatar'>
       <div>
-        <button type='text' onClick={onCreateModal}>+ Add clothes</button>
+        <button className='header__button' type='text' onClick={onCreateModal}>+ Add clothes</button>
       </div>
-      <div>Terrence Tegegne</div>
-      <div>
-        <img src={require('../images/header-avatar.svg').default} alt='avatar'></img>
+      <div className='header__name'>Terrence Tegegne</div>
+      <div className='header__avatar'>
+        <img className='header__avatar-image' src={require('../images/header-avatar.svg').default} alt='avatar'></img>
       </div>
     </div>
   </header>
