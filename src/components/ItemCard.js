fbf95791ab919a4b47495e8858ee94644dd56c4d
Raeann Lytle <raelytle@gmail.com>
2023-05-01 19:57:17 -0400

    working on key prop issue

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,12 +1,12 @@
 import "../blocks/ItemCard.css";
 
-const ItemCard = ({ item, onSelectCard }) => {
+const ItemCard = ({ name, weather, _id, id, item, link, onSelectCard }) => {
   return (
     <div className="card__container">
       <img
         src={item.link}
         className="card_image"
-        onClick={() => onSelectCard(item)}
+        onClick={() => onSelectCard(item, name, weather, _id, id, link)}
         alt='Clothing Item Image'
       />
       <div className="card_name">{item.name}</div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -26,7 +26,15 @@ function Main({ weatherTemp, onSelectCard }) {
         Today is {weatherTemp}°F / You may want to wear:
         <div className="card_items">
           {filteredCards.map((item) => (
-            <ItemCard key={item._id} item={item} onSelectCard={onSelectCard} />
+            <ItemCard
+              key={item._id}
+              item={item}
+              onSelectCard={onSelectCard}
+              name={item.name}
+              weather={item.weather}
+              id={item.id}
+              link={item.link}
+            />
           ))}
         </div>
       </section>
