63085e27402da64ed5004117629f01643938a04b
Raeann Lytle <raelytle@gmail.com>
2023-05-01 16:47:42 -0400

    fix formatting with prettier

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,37 @@
-import '../blocks/Header.css';
-import headerLogo from '../images/header-logo.svg';
-import headerAvatar from '../images/header-avatar.svg';
+import "../blocks/Header.css";
+import headerLogo from "../images/header-logo.svg";
+import headerAvatar from "../images/header-avatar.svg";
 
-const Header = ({onCreateModal}) => {
-  return(
-    <header className='header'>
-    <div className='header__logo'>
-      <div>
-        <img src={headerLogo} alt='logo'></img>
+const Header = ({ onCreateModal }) => {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <div>
+          <img src={headerLogo} alt="logo"></img>
+        </div>
+        <div className="header__date">May 1, Charlotte</div>
       </div>
-      <div className='header__date'>May 1, Charlotte</div>
-    </div>
-    <div className='header__avatar'>
-      <div>
-        <button className='header__button' type='text' onClick={onCreateModal}>+ Add clothes</button>
+      <div className="header__avatar">
+        <div>
+          <button
+            className="header__button"
+            type="text"
+            onClick={onCreateModal}
+          >
+            + Add clothes
+          </button>
+        </div>
+        <div className="header__name">Raeann Lytle</div>
+        <div className="header__avatar">
+          <img
+            className="header__avatar-image"
+            src={headerAvatar}
+            alt="avatar"
+          ></img>
+        </div>
       </div>
-      <div className='header__name'>Raeann Lytle</div>
-      <div className='header__avatar'>
-        <img className='header__avatar-image' src={headerAvatar} alt='avatar'></img>
-      </div>
-    </div>
-  </header>
-  )  
-}
+    </header>
+  );
+};
 
 export default Header;
