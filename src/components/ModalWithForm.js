d87aecb70dba3c140c5d6eb569c6495e5e4584e0
Raeann Lytle <raelytle@gmail.com>
2023-05-01 16:56:40 -0400

    remove unnecessary closing button/reformat

--- src/components/ModalWithForm.js
+++ src/components/ModalWithForm.js
@@ -1,22 +1,28 @@
-import '../blocks/ModalWithForm.css';
+import "../blocks/ModalWithForm.css";
 
-const ModalWithForm = ({ 
-  children, 
-  buttonText = 'Add garment', 
-  title, 
-  onClose, 
-  name 
+const ModalWithForm = ({
+  children,
+  buttonText = "Add garment",
+  title,
+  onClose,
+  name,
 }) => {
   return (
-  <div className={`modal modal_type_${name}`}>
-    <div className='modal__content'>
-      <button type='button' onClick={onClose} className='modal__button-close'></button>
-      <h3 className='modal__title'>{title}</h3>
-      <form>{children}</form>
-      <button type='submit' className='modal__button-submit'>{buttonText}</button>
+    <div className={`modal modal_type_${name}`}>
+      <div className="modal__content">
+        <button
+          type="button"
+          onClick={onClose}
+          className="modal__button-close"
+        />
+        <h3 className="modal__title">{title}</h3>
+        <form>{children}</form>
+        <button type="submit" className="modal__button-submit">
+          {buttonText}
+        </button>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
 export default ModalWithForm;
