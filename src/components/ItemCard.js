fcabcad9b883d40286e9f66e7218c6fc43977374
Raeann Lytle <raelytle@gmail.com>
2023-05-01 16:48:56 -0400

    wrap return in parentheses/format with prettier

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,10 +1,16 @@
-import '../blocks/ItemCard.css';
+import "../blocks/ItemCard.css";
 
 const ItemCard = ({ item, onSelectCard }) => {
-    return <div className='card__container'>
-        <img src={item.link} className='card_image' onClick={() => onSelectCard(item)}/>
-      <div className='card_name'>{item.name}</div>
-    </div>;
-  }
+  return (
+    <div className="card__container">
+      <img
+        src={item.link}
+        className="card_image"
+        onClick={() => onSelectCard(item)}
+      />
+      <div className="card_name">{item.name}</div>
+    </div>
+  );
+};
 
 export default ItemCard;
