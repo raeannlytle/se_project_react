5dee1041332f6cc588e991001ed57da3cddfd95b
Raeann Lytle <raelytle@gmail.com>
2023-05-01 13:48:44 -0400

    fix centering of card names

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,10 +1,8 @@
 import '../blocks/ItemCard.css';
 
 const ItemCard = ({ item, onSelectCard }) => {
-    return <div>
-      <div>
+    return <div className='card__container'>
         <img src={item.link} className='card_image' onClick={() => onSelectCard(item)}/>
-      </div>
       <div className='card_name'>{item.name}</div>
     </div>;
   }
