79a9ed4bd9976620ee898adeb0693603e5bde3d4
Raeann Lytle <raelytle@gmail.com>
2023-05-01 16:51:42 -0400

    reformat using prettier

--- src/components/ItemModal.js
+++ src/components/ItemModal.js
@@ -1,18 +1,22 @@
-import '../blocks/ItemModal.css';
+import "../blocks/ItemModal.css";
 
 const ItemModal = ({ selectedCard, onClose }) => {
   return (
-    <div className='item__modal'>
-      <div className='item__modal-container'>
-      <button type='button' onClick={onClose} className="modal__button-close"></button>
-        <img src={selectedCard.link} className="item__modal-image"/>
-        <div className='item__modal-caption'>
-        <div>{selectedCard.name}</div>
-        <div>Weather: {selectedCard.weather}</div>
+    <div className="item__modal">
+      <div className="item__modal-container">
+        <button
+          type="button"
+          onClick={onClose}
+          className="modal__button-close"
+        ></button>
+        <img src={selectedCard.link} className="item__modal-image" />
+        <div className="item__modal-caption">
+          <div>{selectedCard.name}</div>
+          <div>Weather: {selectedCard.weather}</div>
         </div>
       </div>
-    </div>  
-  )
-}
+    </div>
+  );
+};
 
 export default ItemModal;
