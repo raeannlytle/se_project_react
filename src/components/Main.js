4f4525a6b44e24de626339a9f5362a76d780693e
Raeann Lytle <raelytle@gmail.com>
2023-05-01 16:49:26 -0400

    reformat with prettier

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,32 +1,32 @@
 import { defaultClothingItems } from "../utils/constants";
-import WeatherCard from './WeatherCard';
-import ItemCard from './ItemCard';
-import '../blocks/Main.css';
-import { useMemo } from 'react';
+import WeatherCard from "./WeatherCard";
+import ItemCard from "./ItemCard";
+import "../blocks/Main.css";
+import { useMemo } from "react";
 
 function Main({ weatherTemp, onSelectCard }) {
   const weatherType = useMemo(() => {
     if (weatherTemp >= 86) {
-      return 'hot';
+      return "hot";
     } else if (weatherTemp >= 66 && weatherTemp <= 85) {
-      return 'warm';
+      return "warm";
     } else if (weatherTemp <= 65) {
-      return 'cold';
+      return "cold";
     }
   }, [weatherTemp]);
 
   const filteredCards = defaultClothingItems.filter((item) => {
-    return item.weather.toLowerCase() === weatherType
-  })
+    return item.weather.toLowerCase() === weatherType;
+  });
 
   return (
-  <main className='main'>
-    <WeatherCard day={true} type='sunny' weatherTemp={weatherTemp} />
-      <section className='card_section' id='card-section'>
+    <main className="main">
+      <WeatherCard day={true} type="sunny" weatherTemp={weatherTemp} />
+      <section className="card_section" id="card-section">
         Today is {weatherTemp}°F / You may want to wear:
-        <div className='card_items'>
+        <div className="card_items">
           {filteredCards.map((item) => (
-            <ItemCard item={item} onSelectCard={onSelectCard}/>
+            <ItemCard item={item} onSelectCard={onSelectCard} />
           ))}
         </div>
       </section>
