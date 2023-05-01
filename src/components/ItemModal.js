0a707f560708fc3899ad6935a78fce735a92192f
Raeann Lytle <raelytle@gmail.com>
2023-05-01 13:29:55 -0400

    add ItemModal styling

--- src/components/ItemModal.js
+++ src/components/ItemModal.js
@@ -1,11 +1,15 @@
+import '../blocks/ItemModal.css';
+
 const ItemModal = ({ selectedCard, onClose }) => {
   return (
-    <div className='modal'>
-      <div className='modal__content'>
+    <div className='item__modal'>
+      <div className='item__modal-container'>
       <button type='button' onClick={onClose} className="modal__button-close"></button>
-        <img src={selectedCard.link} className="modal__image-preview"/>
-        <div className="modal__preview-text">{selectedCard.name}</div>
+        <img src={selectedCard.link} className="item__modal-image"/>
+        <div className='item__modal-caption'>
+        <div>{selectedCard.name}</div>
         <div>Weather: {selectedCard.weather}</div>
+        </div>
       </div>
     </div>  
   )
