67d64ab46948d98fd628c5d7986f1e94cc8ddedd
Raeann Lytle <raelytle@gmail.com>
2023-05-19 08:50:34 -0400

    fix error

--- src/contexts/CurrentTemperatureUnitContext.js
+++ src/contexts/CurrentTemperatureUnitContext.js
@@ -1,5 +1,8 @@
 import React from "react";
 
-const CurrentTemperatureUnitContext = React.createContext();
+const CurrentTemperatureUnitContext = React.createContext({
+  currentTempUnit: "",
+  handleToggleSwitchChange: () => {},
+});
 
 export { CurrentTemperatureUnitContext };
