ebb7281ea205c4002e54c91bd21729c55fc52c30
Raeann Lytle <raelytle@gmail.com>
2023-11-27 18:43:35 -0500

    fix header.js and errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,135 +1,85 @@
 import "../blocks/Header.css";
 import headerLogo from "../images/header-logo.svg";
-import headerAvatar from "../images/header-avatar.svg";
 import ToggleSwitch from "./ToggleSwitch";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
-import { useContext } from "react";
-import { useState } from "react";
-import LoginModal from "./LoginModal";
-import RegisterModal from "./RegisterModal";
-import * as api from "../utils/api";
+import { useContext, useState } from "react";
 
-const Header = ({ onCreateModal, setCurrentUser, setIsLoggedIn }) => {
+const Header = ({
+  onCreateModal,
+  weatherLocation,
+  onLoginModal,
+  onRegisterModal,
+  isLoggedIn,
+}) => {
+  const currentUser = useContext(CurrentUserContext);
+  const avatar = currentUser ? currentUser.avatar : undefined;
+  const showAvatar = avatar !== "" ? true : false;
+  const username = currentUser.username;
   const currentDate = new Date().toLocaleString("default", {
     month: "long",
     day: "numeric",
   });
 
-  const { currentUser, isLoggedIn } = useContext(CurrentUserContext);
-
-  const avatarPlaceholder = currentUser
-    ? currentUser.username.charAt(0).toUpperCase()
-    : "";
-
-  const [isLoginModalOpen, setLoginModalOpen] = useState(false);
-  const [isRegisterModalOpen, setRegisterModalOpen] = useState(false);
-
-  const toggleLoginModal = () => {
-    setLoginModalOpen(true);
-  };
-
-  const toggleRegisterModal = () => {
-    setRegisterModalOpen(!isRegisterModalOpen);
-  };
-
-  const handleLogin = () => {
-    const userLogin = {
-      email: "",
-      password: "",
-    };
-
-    api
-      .loginUser(userLogin)
-      .then((res) => {
-        console.log("Login response:", res);
-        if (res.access) {
-          localStorage.setItem("jwt", res.token);
-          setCurrentUser({
-            id: res.user.id,
-            username: res.user.username,
-            email: res.user.email,
-          });
-          setIsLoggedIn(true);
-        } else {
-          console.error("Login failed. Server did not provide access");
-        }
-      })
-      .catch((e) => {
-        console.error("Login error:", e);
-      });
-  };
+  // ... (previous code)
 
   return (
     <header className="header">
-      <div className="header__logo">
-        <NavLink exact="true" to="/">
-          <div>
-            <img src={headerLogo} alt="logo"></img>
-          </div>
-        </NavLink>
-        <div className="header__date">{currentDate}, Charlotte</div>
-      </div>
-      <div className="header__avatar">
-        <div className="header__slider"></div>
-        <ToggleSwitch />
-        <div>
-          <button
-            className="header__button"
-            type="text"
-            onClick={onCreateModal}
-          >
-            + Add clothes
-          </button>
-        </div>
-        <NavLink to="/profile">
-          {currentUser && (
-            <>
-              <div className="header__name">{currentUser.username}</div>
-              <div className="header__avatar">
-                {currentUser.avatar ? (
-                  <img
-                    className="header__avatar-image"
-                    src={currentUser.avatar}
-                    alt="avatar"
-                  ></img>
-                ) : (
-                  <div className="avatar-placeholder">{avatarPlaceholder}</div>
-                )}
-              </div>
-            </>
-          )}
-        </NavLink>
-        {!isLoggedIn ? (
-          <>
+      <div className="header__menu-left">
+        <div className="header__logo">
+          <Link to="/">
             <div>
-              <button
-                className="header__button"
-                type="text"
-                onClick={toggleLoginModal}
-              >
-                Login
-              </button>
-              {isLoginModalOpen && (
-                <LoginModal onClose={toggleLoginModal} onLogin={handleLogin} />
-              )}
-              <button
-                className="header__button"
-                type="text"
-                onClick={toggleRegisterModal}
-              >
-                Register
-              </button>
-              {isRegisterModalOpen && (
-                <RegisterModal
-                  onClose={() => setRegisterModalOpen(false)}
-                  onRegister={() => setRegisterModalOpen(false)}
-                />
-              )}
+              <img src={headerLogo} alt="logo"></img>
             </div>
-          </>
-        ) : null}
+          </Link>
+        </div>
       </div>
+      {isLoggedIn ? (
+        <div className="header__menu-right">
+          <ToggleSwitch />
+          <div className="header__menu-buttons">
+            <button
+              className="header__add-button"
+              onClick={onCreateModal}
+              type="text"
+            >
+              + Add Clothes
+            </button>
+          </div>
+          <Link className="header__name" to="/profile">
+            {currentUser.username}
+          </Link>
+          <div>
+            {showAvatar ? (
+              <img className="header__avatar" src={avatar} alt="avatar" />
+            ) : (
+              <p className="header__avatar-placeholder">
+                {username[0]?.toUpperCase()}
+              </p>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div className="header__menu-right">
+          <ToggleSwitch />
+          <div className="header__menu-buttons">
+            <button
+              className="header__add-button"
+              onClick={onRegisterModal}
+              type="text"
+            >
+              Register
+            </button>
+            <button
+              className="header__add-button"
+              onClick={onLoginModal}
+              type="text"
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
