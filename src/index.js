cc66675c764c403de536e56476a13916eb293f1b
Raeann Lytle <raelytle@gmail.com>
2023-11-27 13:38:13 -0500

    add missing files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App.js";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+reportWebVitals();
