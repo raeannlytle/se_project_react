6b90b0722a473ca2b0de0ea3685e8fe84701d163
Raeann Lytle <raelytle@gmail.com>
2023-11-27 19:47:26 -0500

    update sidebar for edit and logout

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,39 @@
-import avatar from "../images/header-avatar.svg";
+import React, { useContext } from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import "../blocks/SideBar.css";
 
-function SideBar() {
+function SideBar({ onEditProfileModal, onLogout }) {
+  const currentUser = useContext(CurrentUserContext);
+  const handleLogout = () => {
+    onLogout();
+  };
+
   return (
-    <div className="sidebar">
-      <img src={avatar} className="sidebar__avatar" alt={avatar} />
-      <div className="sidebar__name">Raeann Lytle</div>
+    <div className="profile__sidebar">
+      <div className="profile__sidebar-avatar-name">
+        <img
+          className="profile__avatar"
+          src={currentUser.avatar}
+          alt={currentUser.username}
+        ></img>
+        <div className="profile__name">{currentUser.username}</div>
+      </div>
+      <div className="profile__sidebar-button-container">
+        <button
+          type="button"
+          className="profile__sidebar-button"
+          onClick={onEditProfileModal}
+        >
+          Edit Profile
+        </button>
+        <button
+          className="profile__sidebar-button"
+          type="button"
+          onClick={handleLogout}
+        >
+          Logout
+        </button>
+      </div>
     </div>
   );
 }
