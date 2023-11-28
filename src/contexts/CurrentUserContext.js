c5dcda506e7d19f8babd709b50a76a2b756e1742
Raeann Lytle <raelytle@gmail.com>
2023-11-27 19:20:51 -0500

    update api relevant code

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,18 +1,3 @@
-import React, { createContext, useState } from "react";
+import React from "react";
 
-const CurrentUserContext = createContext(null);
-
-const CurrentUserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  return (
-    <CurrentUserContext.Provider
-      value={{ currentUser, isLoggedIn, setCurrentUser, setIsLoggedIn }}
-    >
-      {children}
-    </CurrentUserContext.Provider>
-  );
-};
-
-export { CurrentUserContext, CurrentUserProvider };
+export const CurrentUserContext = React.createContext({ currentUser: "" });
