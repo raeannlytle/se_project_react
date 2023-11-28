1264e39a76816d5fd3ade1354f72b944dfcd72e1
Raeann Lytle <raelytle@gmail.com>
2023-11-27 19:02:43 -0500

    update profile for edit profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,73 +1,29 @@
 import SideBar from "./SideBar";
 import ClothesSection from "./ClothesSection";
 import "../blocks/Profile.css";
-import ItemCard from "./ItemCard";
-import "../blocks/ItemCards.css";
-import EditProfileModal from "./EditProfileModal";
-import { CurrentUserContext } from "../contexts/CurrentUserContext";
-import { useState, useContext } from "react";
+import React, { useState, useContext } from "react";
 
-function Profile({
-  items,
-  onSelectedCard,
+const Profile = ({
   onCreateModal,
-  onSignOut,
-  onCardClick,
-}) {
-  const [isEditProfileModalOpen, setEditProfileModalOpen] = useState(false);
-  const { currentUser } = useContext(CurrentUserContext);
-
-  const handleEditProfile = () => {
-    setEditProfileModalOpen(true);
-  };
-
-  const handleCloseEditProfileModal = () => {
-    setEditProfileModalOpen(false);
-  };
-
-  const handleSignOut = () => {
-    onSignOut();
-  };
-
+  clothingItems,
+  onSelectedCard,
+  onEditProfileModal,
+  onLogout,
+  onCardLike,
+}) => {
   return (
-    <div className="profile">
+    <section className="profile">
       <div className="profile__section">
-        <SideBar />
+        <SideBar onEditProfileModal={onEditProfileModal} onLogout={onLogout} />
       </div>
-      <div className="profile__clothes">
-        <ClothesSection
-          cards={items}
-          onCreateModal={onCreateModal}
-          onCardClick={onCardClick}
-        />
-        <section className="cards">
-          <ul className="cards__list">
-            {items.map((card) => (
-              <ItemCard
-                key={card._id}
-                item={card}
-                name={card.name}
-                onSelectedCard={onSelectedCard}
-                id={card.id}
-                weather={card.weather}
-                link={card.link}
-                onCardClick={onCardClick}
-              />
-            ))}
-          </ul>
-        </section>
-      </div>
-      <button className="profile__edit-button" onClick={handleEditProfile}>
-        Edit Profile
-      </button>
-      <button className="profile__sign-out-button" onClick={handleSignOut}>
-        Sign out
-      </button>
-      {isEditProfileModalOpen && (
-        <EditProfileModal onClose={handleCloseEditProfileModal} />
-      )}
-    </div>
+      <ClothesSection
+        onSelectedCard={onSelectedCard}
+        clothingItems={clothingItems}
+        onCreateModal={onCreateModal}
+        onCardLike={onCardLike}
+      />
+    </section>
   );
-}
+};
 
 export default Profile;
