64e72a11885915953e0840035bee582495431a36
Raeann Lytle <raelytle@gmail.com>
2023-11-27 19:29:38 -0500

    update authorization and move add/remove to auth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,61 +1,67 @@
-const API_URL = "http://localhost:3001";
+import { baseUrl } from "./constants";
+import { checkResponse } from "./utils";
 
-export const registerUser = async ({ name, avatar, email, password }) => {
-  try {
-    const response = await fetch(`${API_URL}/signup`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name, avatar, email, password }),
-    });
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error signing up user:", error);
-    throw error;
-  }
+export const registerUser = ({ email, password, username, avatar }) => {
+  return fetch(`${baseUrl}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ email, password, username, avatar }),
+  }).then(checkResponse);
 };
 
-export const loginUser = async ({ email, password }) => {
-  try {
-    const response = await fetch(`${API_URL}/signin`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    });
-
-    if (!response.ok) {
-      const errorData = await response.json();
-      console.error("Error signing in user:", errorData);
-      throw new Error(errorData.message || "Authentication failed");
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error signing in user:", error);
-    throw error;
-  }
+export const loginUser = ({ email, password }) => {
+  return fetch(`${baseUrl}/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then(checkResponse);
 };
 
-export const checkToken = async (token) => {
-  try {
-    const response = await fetch(`${API_URL}/users/me`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
+export const getUserInfo = (token) => {
+  return fetch(`${baseUrl}/users/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  }).then(checkResponse);
+};
 
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error checking token validity:", error);
-    throw error;
-  }
+export const editProfile = (username, avatar, token) => {
+  return fetch(`${baseUrl}/users/me`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ username, avatar }),
+  }).then(checkResponse);
+};
+
+export const addCardLike = (itemId, userId, token) => {
+  return fetch(`${baseUrl}/items/${itemId}/likes`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ itemId, userId }),
+  }).then(checkResponse);
+};
+
+export const removeCardLike = (itemId, userId, token) => {
+  return fetch(`${baseUrl}/items/${itemId}/likes`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ itemId, userId }),
+  }).then(checkResponse);
 };
