85ae7afac45fe592342bebf1fc4756901b8f8ab2
Raeann Lytle <raelytle@gmail.com>
2023-12-01 10:34:59 -0500

    fix spacing of title and header

--- src/components/ClothesSection.js
+++ src/components/ClothesSection.js
@@ -12,34 +12,35 @@ function ClothesSection({
 }) {
   const currentUser = useContext(CurrentUserContext);
   const currentItems = clothingItems.filter((item) => {
-    return item.owner === currentUser.data?._id;
+    return item.owner === currentUser._id;
   });
 
   return (
     <div className="clothes">
-      <div className="clothes__title">Your items</div>
-      <button
-        className="clothes__button"
-        type="submit"
-        onClick={handleCreateModal}
-      >
-        + Add new
-      </button>
+      <div className="clothes__header">
+        <div className="clothes__title">Your items</div>
+        <button
+          className="clothes__button"
+          type="submit"
+          onClick={handleCreateModal}
+        >
+          + Add new
+        </button>
+      </div>
       <div className="clothes__items">
-        {currentItems.map((item) => {
-          return (
-            <ItemCard
-              key={item?._id ?? item?.id}
-              item={item}
-              onSelectedCard={onSelectedCard}
-              isLoggedIn={isLoggedIn}
-              onCardLike={onCardLike}
-            />
-          );
-        })}
+        {currentItems.map((item) => (
+          <ItemCard
+            key={item?._id ?? item?.id}
+            item={item}
+            onSelectedCard={onSelectedCard}
+            isLoggedIn={isLoggedIn}
+            onCardLike={onCardLike}
+          />
+        ))}
       </div>
     </div>
   );
 }
 
 export default ClothesSection;
+
