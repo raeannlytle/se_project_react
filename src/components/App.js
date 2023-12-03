51aa1ac306692e1d254a2500c22524cbd3e984d2
Raeann Lytle <raelytle@gmail.com>
2023-12-03 14:49:55 -0500

    fix delete item not showing up

--- src/components/App.js
+++ src/components/App.js
@@ -234,7 +234,7 @@ function App() {
       const token = localStorage.getItem("jwt");
       getUserInfo(token)
         .then((userData) => {
-          setCurrentUser(userData);
+          setCurrentUser(userData.data);
         })
         .catch(console.error);
     }
