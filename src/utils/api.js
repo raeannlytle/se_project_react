887906254c2ce46f1cd080f9b43a785d7ef01bca
Raeann Lytle <raelytle@gmail.com>
2023-12-28 10:51:40 -0500

    restart sprint 15

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://api.wtwr.customintegrator.org"
+    ? ""
     : "http://localhost:3001";
 
 export const checkResponse = (res) => {
